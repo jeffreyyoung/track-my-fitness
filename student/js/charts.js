406bff145d6f469461b037d63965222ead972610
Christian Carnley <ccarnley7@gmail.com>
2014-12-07 22:57:49 -0700

    added the charts for the charts page

---
+++ student/js/charts.js
@@ -0,0 +1,35 @@
+$(function () {
+    $('#container').highcharts({
+        title: {
+            text: 'Bench',
+            x: 0 //center
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr']
+        },
+        credits: {
+          enabled: false
+        },
+        yAxis: {
+            title:{
+                text: 'Predicted 1 Reb Max (lbs)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#BAD5F6'
+            }]
+        },
+        tooltip: {
+            enabled: false
+        },
+        exporting: {
+         enabled: false
+        },
+        series: [{
+            name: 'Tokyo',
+            showInLegend: false,
+            data: [100, 120, 120, 150]
+        }]
+    });
+});
