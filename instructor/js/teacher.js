e2569a53e4fb632f26aea815ae8650f9ad65ebb5
Christian Carnley <ccarnley7@gmail.com>
2014-12-08 12:33:22 -0700

    charts for teacher view

---
+++ instructor/js/teacher.js
@@ -0,0 +1,143 @@
+$(function () {
+    $('#chart1').highcharts({
+        title: {
+            text: 'Bench',
+            x: 0 //center
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr']
+        },
+        credits: {
+          enabled: false
+        },
+        yAxis: {
+            title:{
+                text: 'Predicted 1 Reb Max (lbs)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#BAD5F6'
+            }]
+        },
+        tooltip: {
+            enabled: false
+        },
+        exporting: {
+         enabled: false
+        },
+        series: [{
+            name: 'Bench',
+            showInLegend: false,
+            data: [100, 120, 120, 150]
+        }]
+    });
+});
+
+$(function () {
+    $('#chart2').highcharts({
+        title: {
+            text: 'Bench',
+            x: 0 //center
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr']
+        },
+        credits: {
+          enabled: false
+        },
+        yAxis: {
+            title:{
+                text: 'Predicted 1 Reb Max (lbs)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#BAD5F6'
+            }]
+        },
+        tooltip: {
+            enabled: false
+        },
+        exporting: {
+         enabled: false
+        },
+        series: [{
+            name: 'Bench',
+            showInLegend: false,
+            data: [100, 120, 120, 150]
+        }]
+    });
+});
+
+$(function () {
+    $('#chart3').highcharts({
+        title: {
+            text: 'Bench',
+            x: 0 //center
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr']
+        },
+        credits: {
+          enabled: false
+        },
+        yAxis: {
+            title:{
+                text: 'Predicted 1 Reb Max (lbs)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#BAD5F6'
+            }]
+        },
+        tooltip: {
+            enabled: false
+        },
+        exporting: {
+         enabled: false
+        },
+        series: [{
+            name: 'Bench',
+            showInLegend: false,
+            data: [100, 120, 120, 150]
+        }]
+    });
+});
+
+$(function () {
+    $('#chart4').highcharts({
+        title: {
+            text: 'Bench',
+            x: 0 //center
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr']
+        },
+        credits: {
+          enabled: false
+        },
+        yAxis: {
+            title:{
+                text: 'Predicted 1 Reb Max (lbs)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#BAD5F6'
+            }]
+        },
+        tooltip: {
+            enabled: false
+        },
+        exporting: {
+         enabled: false
+        },
+        series: [{
+            name: 'Bench',
+            showInLegend: false,
+            data: [100, 120, 120, 150]
+        }]
+    });
+});
