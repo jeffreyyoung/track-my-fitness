06d18f6b6f5db907d9460f7ba67a542d82570c15
Jeffrey Young <jeffreeyoung@yahoo.com>
2014-12-17 23:20:52 -0700

    renaming stuff

--- instructor-old/js/accordion.js
+++ instructor-old/js/accordion.js
@@ -1,29 +1,16 @@
 var accordions = document.querySelectorAll(".accordion section")
 var lastClicked;
-var h2s = document.querySelectorAll('.accordion > section > h2')
 for (var i = 0; i < accordions.length; i++) {
     var accord = accordions[i]
     var h2 = accord.childNodes[1]
     h2.addEventListener('click', function () {
-        unselectH2s(h2s)
-        this.classList.add('selected')
         var ul = this.parentNode.childNodes[3]
         if (lastClicked && lastClicked.classList.contains("show") && lastClicked !== ul) {
             lastClicked.classList.remove("show")
         }
-        var text = generateSectionHtml(this.innerHTML)
-        updateMain(text)
         lastClicked = ul
-        unselectAllStudents(students)
         ul.classList.toggle('show')
     })
-}
-
-function unselectH2s(h2s){
-    for( var i = 0; i < h2s.length; i++){
-        var h2 = h2s[i];
-        h2.classList.remove('selected')
-    }
 }
 
 var students = document.querySelectorAll('ul.accordion.vertical section ul li')
@@ -34,26 +21,20 @@ for (var i = 0; i < students.length; i++) {
         unselectAllStudents(students)
         console.log(this)
         this.classList.add('selected')
-        var text = generateStudentHtml(this.innerHTML)
+        var text = this.innerHTML
         updateMain(text)
+
     });
 }
 
 function unselectAllStudents(students) {
     for (var i = 0; i < students.length; i++) {
         students[i].classList.remove('selected')
+
     }
 }
 
 
 function updateMain(html) {
-    var $main = document.querySelector('#main')
-    $main.classList.add('hidden')
-    setTimeout(function(){ 
-        $main.innerHTML = html
-        $main.classList.remove('hidden')
-        $main.classList.add('visible') 
-        buildCharts()
-    }, 200);
-    
+    document.querySelector('#main').innerHTML = html
 }
--- instructor/js/accordion.js
+++ instructor/js/accordion.js
@@ -1,16 +1,29 @@
 var accordions = document.querySelectorAll(".accordion section")
 var lastClicked;
+var h2s = document.querySelectorAll('.accordion > section > h2')
 for (var i = 0; i < accordions.length; i++) {
     var accord = accordions[i]
     var h2 = accord.childNodes[1]
     h2.addEventListener('click', function () {
+        unselectH2s(h2s)
+        this.classList.add('selected')
         var ul = this.parentNode.childNodes[3]
         if (lastClicked && lastClicked.classList.contains("show") && lastClicked !== ul) {
             lastClicked.classList.remove("show")
         }
+        var text = generateSectionHtml(this.innerHTML)
+        updateMain(text)
         lastClicked = ul
+        unselectAllStudents(students)
         ul.classList.toggle('show')
     })
+}
+
+function unselectH2s(h2s){
+    for( var i = 0; i < h2s.length; i++){
+        var h2 = h2s[i];
+        h2.classList.remove('selected')
+    }
 }
 
 var students = document.querySelectorAll('ul.accordion.vertical section ul li')
@@ -21,20 +34,26 @@ for (var i = 0; i < students.length; i++) {
         unselectAllStudents(students)
         console.log(this)
         this.classList.add('selected')
-        var text = this.innerHTML
+        var text = generateStudentHtml(this.innerHTML)
         updateMain(text)
-
     });
 }
 
 function unselectAllStudents(students) {
     for (var i = 0; i < students.length; i++) {
         students[i].classList.remove('selected')
-
     }
 }
 
 
 function updateMain(html) {
-    document.querySelector('#main').innerHTML = html
+    var $main = document.querySelector('#main')
+    $main.classList.add('hidden')
+    setTimeout(function(){ 
+        $main.innerHTML = html
+        $main.classList.remove('hidden')
+        $main.classList.add('visible') 
+        buildCharts()
+    }, 200);
+    
 }
